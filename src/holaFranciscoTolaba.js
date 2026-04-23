0595340db1ebcbfb522fe09e2d931ade05f7a148
FranciscoTolaba <franciscotolaba66@gmail.com>
2026-04-23 15:53:16 -0300

    Agregado de codigo al archivo js

---
+++ src/holaFranciscoTolaba.js
@@ -0,0 +1,13 @@
+// no usar
+var nombre ="Francisco";
+//trabajar con let y const
+let apellido ="Tolaba";
+//const texto = "Hola Mundo, segun ";
+const TEXTO = ` Hola Mundo, según ${nombre} ${apellido}`;
+//const TEXTO = 'Hola Mundo, según ${nombre}${apellido}';
+
+//console.log("Hola Mundo, segun "+nombre+apellido);
+
+//console.log(TEXTO + nombre + apellido);
+
+console.log( TEXTO);
