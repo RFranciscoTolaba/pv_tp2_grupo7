6811248daabc1e34c8fae907552aa1e999a9dd13
Lucas <lucassantiagomn@gmail.com>
2026-04-29 15:48:34 -0300

    desarrollo de eejrcicio 7 y 10

---
+++ src/holaLucasSegovia.js
@@ -0,0 +1,14 @@
+//no usar
+//var nombre ="Lucas";
+//trabajar con let y const
+let apellido ="Segovia";
+//const texto = "Hola Mundo, segun ";
+const TEXTO = "ES MAYOR DE EDAD";
+if (edad===18) {
+    var nombre = "Lucas";
+    console.log("Es mayor de edad");
+} 
+
+//console.log(TEXTO + nombre + apellido);
+
+console.log( TEXTO);
