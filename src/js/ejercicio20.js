0bbb30dc071c9912960bf0f445a5d3a077df3177
Gerardo <gera.reyes578@gmail.com>
2026-05-01 00:11:24 -0300

    guardo ultimos cambios - gerardo reyes

--- src/js/ejercicio20.js
+++ src/js/ejercicio20.js
@@ -1,10 +1,10 @@
 // logica de negocio
 import { guardarEstudianteEnArray, mostrarEstudiante } from "../services/serivesEjercicio20.js";
 import { serviceFranciscoTolaba } from "../services/serviceFranciscoTolaba.js";
-import { servicioLucasSegovia } from "../service/serviceLucasSegovia.js";
+import { servicioLucasSegovia } from "../services/serviceLucasSegovia.js";
 import { serviceRicardoCondori } from "../services/serviceRicardoCondori.js"
 import { serviceGerardoReyes } from "../services/serviceGerardoReyes.js";
-import { servicioLlampaKevin } from "../service/serviceLlampaKevin.js";
+import { servicioLlampaKevin } from "../services/serviceLlampaKevin.js";
 // Esperar al DOM
 document.addEventListener('DOMContentLoaded', () => {
     
